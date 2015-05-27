e2cde0d0c88746c6c5ef37851d8f8b8ab4890c87
Jonathan Samples <jonathan.samples@g5searchmarketing.com>
2015-05-27 14:09:46 -0700

    Added click jumping

--- src/gamescript.js
+++ src/gamescript.js
@@ -43,6 +43,13 @@ var GameLayer = cc.Layer.extend({
             },
             onKeyReleased: function(keyCode, event){}
         }, this);
+        cc.eventManager.addListener({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            swallowTouches: true,
+            onTouchBegan: function(touch, event){
+                totemSprite.jump();
+            }
+        }, this);
     },
     // Do stuff for each step
     update: function(dt){
